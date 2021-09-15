ea5fecfbb27267786f23acb7792c9328ce96c070
danbudris <budris@amazon.com>
2021-09-15 19:39:58 -0400

    if interative=false, don't spin

--- cmd/troubleshoot/cli/run.go
+++ cmd/troubleshoot/cli/run.go
@@ -109,6 +109,7 @@ func runTroubleshoot(v *viper.Viper, arg string) error {
 	}
 
 	s := spin.New()
+	interactive := v.GetBool("interactive") && isatty.IsTerminal(os.Stdout.Fd())
 	finishedCh := make(chan bool, 1)
 	progressChan := make(chan interface{}) // non-zero buffer can result in missed messages
 	isFinishedChClosed := false
@@ -128,9 +129,9 @@ func runTroubleshoot(v *viper.Viper, arg string) error {
 				fmt.Printf("\r%s\r", cursor.ClearEntireLine())
 				return
 			case <-time.After(time.Millisecond * 100):
-				if currentDir == "" {
+				if currentDir == "" && interactive {
 					fmt.Printf("\r%s \033[36mCollecting support bundle\033[m %s", cursor.ClearEntireLine(), s.Next())
-				} else {
+				} else if interactive {
 					fmt.Printf("\r%s \033[36mCollecting support bundle\033[m %s %s", cursor.ClearEntireLine(), s.Next(), currentDir)
 				}
 			}
@@ -177,8 +178,6 @@ func runTroubleshoot(v *viper.Viper, arg string) error {
 		// Don't die
 	} else if len(analyzeResults) > 0 {
 
-		interactive := v.GetBool("interactive") && isatty.IsTerminal(os.Stdout.Fd())
-
 		if interactive {
 			close(finishedCh) // this removes the spinner
 			isFinishedChClosed = true
@@ -194,7 +193,7 @@ func runTroubleshoot(v *viper.Viper, arg string) error {
 				c.Printf("%s\r * Failed to format analysis: %v\n", cursor.ClearEntireLine(), err)
 			}
 
-			fmt.Printf("%s", formatted)
+			fmt.Printf("\n%s\n", formatted)
 		}
 	}
 
